d1c64a44d95fef43eb2423a3b3f0937572a7a86c
jazzisart@gmail.com <jazzisart@gmail.com>
2014-12-26 12:55:43 -0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// -----------------------------------
+// ALL CAPS
+// -----------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+// triggers animations when objects hits a certain distance (px) from the top of window
+
+// $(window).scroll(function() {
+// 	$('.start-anim').each(function(){
+// 		var imagePos = $(this).offset().top;
+// 		var topOfWindow = $(window).scrollTop();
+
+// 		if (imagePos < topOfWindow+500) {
+// 			$(this).children('.left').addClass("animated fadeInLeft");
+// 			$(this).children('.right').addClass("animated fadeInRight");
+// 		}
+// 	});
+// });
+
